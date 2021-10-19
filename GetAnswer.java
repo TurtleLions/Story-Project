bac33f6abf700a06548355136f56b0edad56b665
Ryan Estanislao <>
2021-10-19 15:37:00 +0000

    set GetAnswer

--- GetAnswer.java
+++ GetAnswer.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
+import java.util.concurrent.*;
 public class GetAnswer extends Thread{
-  private String recievedInput = "";
+  private String recievedInput;
   public void run(){
     Scanner input = new Scanner(System.in);
     recievedInput = input.nextLine();
   }
-  public String returnInput(){
-    return recievedInput;
+  public void returns() throws InterruptedException, ExecutionException{
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    Callable<String> callable = new Callable<String>(){
+      @Override
+      public String call(){
+        return recievedInput;
+      }
+    };
+    Future<String> future = executor.submit(callable);
+    executor.shutdown();
   }
 }
